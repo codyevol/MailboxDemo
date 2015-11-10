ad8fa5c1e5e02a5b21f62e0b3d61a495ea007874
Cody Evol <cody@evolcreative.com>
2015-11-09 23:34:07 -0800

    working colors

--- MailboxDemo/MailboxViewController.swift
+++ MailboxDemo/MailboxViewController.swift
@@ -23,6 +23,8 @@ class MailboxViewController: UIViewController, UIScrollViewDelegate {
 
     
     var messageOriginalCenter: CGPoint!
+    var messageSwipeLeft: Bool!
+    var messageSwipeRight: Bool!
     
     
     
@@ -31,18 +33,22 @@ class MailboxViewController: UIViewController, UIScrollViewDelegate {
     
             scrollView.contentSize = imageView.image!.size
             messageOriginalCenter = messageView.frame.origin
-            singleMessage.backgroundColor = UIColor(hue: 123, saturation: 0, brightness: 88, alpha: 100)
-        
-
-    
+             singleMessage.backgroundColor = UIColor(red: 151/255.0, green: 151/255.0, blue: 151/255.0, alpha: 1.0)
+            messageSwipeRight = false
+            messageSwipeLeft = false
+            deleteIcon.alpha = 0
+            archiveIcon.alpha = 0.5
+            listIcon.alpha = 0
+            laterIcon.alpha = 0.5
         
     }
+    
    
     @IBAction func messageScroll(sender: UIPanGestureRecognizer) {
         
         
         let translation = sender.translationInView(view)
-        let velocity = sender.velocityInView(view)
+        //let velocity = sender.velocityInView(view)
         
         
         
@@ -58,12 +64,34 @@ class MailboxViewController: UIViewController, UIScrollViewDelegate {
             
             messageView.center = CGPoint(x: messageOriginalCenter.x + translation.x, y: messageOriginalCenter.y)
             
+            if translation.x > 64 && translation.x < 160{
+                
+                  singleMessage.backgroundColor = UIColor(red: 111/255.0, green: 217/255.0, blue: 98/255.0, alpha: 1.0)
             
+            } else if translation.x > 160{
+                
+                singleMessage.backgroundColor = UIColor(red: 203/255.0, green: 6/255.0, blue: 30/255.0, alpha: 1.0)
+            
+            } else if translation.x < 63 && translation.x > -63 {
+                
+                singleMessage.backgroundColor = UIColor(red: 151/255.0, green: 151/255.0, blue: 151/255.0, alpha: 1.0)
+
+            } else if translation.x < -64  && translation.x > -160{
+                
+                singleMessage.backgroundColor = UIColor(red: 249/255.0, green: 210/255.0, blue: 70/255.0, alpha: 1.0)
+            
+            } else if translation.x < -160 {
+                
+                singleMessage.backgroundColor = UIColor(red: 215/255.0, green: 166/255.0, blue: 120/255.0, alpha: 1.0)
+            }
+
         
         }else if sender.state == UIGestureRecognizerState.Ended {
             
             print("ended")
             
+            }
+       
         }
+        
     }
-}
