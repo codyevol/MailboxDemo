a51c1bb72d51cf17dbb98e9b1c9bc820b82e7736
Cody Evol <cody@evolcreative.com>
2015-11-09 22:22:45 -0800

    Working Slide

--- MailboxDemo/MailboxViewController.swift
+++ MailboxDemo/MailboxViewController.swift
@@ -8,31 +8,62 @@
 
 import UIKit
 
-class MailboxViewController: UIViewController {
+class MailboxViewController: UIViewController, UIScrollViewDelegate {
 
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var singleMessage: UIView!
+    @IBOutlet weak var messageView: UIImageView!
+    @IBOutlet weak var deleteIcon: UIImageView!
+    @IBOutlet weak var archiveIcon: UIImageView!
+    @IBOutlet weak var laterIcon: UIImageView!
+    @IBOutlet weak var listIcon: UIImageView!
+    @IBOutlet weak var leftSwipe: UIView!
+    @IBOutlet weak var rightSwipe: UIView!
+
+    
+    var messageOriginalCenter: CGPoint!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
     
             scrollView.contentSize = imageView.image!.size
-    }
+            messageOriginalCenter = messageView.frame.origin
+            singleMessage.backgroundColor = UIColor(hue: 123, saturation: 0, brightness: 88, alpha: 100)
+        
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        
     }
-    */
+   
+    @IBAction func messageScroll(sender: UIPanGestureRecognizer) {
+        
+        
+        let translation = sender.translationInView(view)
+        let velocity = sender.velocityInView(view)
+        
+        
+        
+        if sender.state == UIGestureRecognizerState.Began {
+           
+            print("began")
+            
+            messageOriginalCenter = messageView.center
 
+        }else if sender.state == UIGestureRecognizerState.Changed {
+           
+            print(translation.x)
+            
+            messageView.center = CGPoint(x: messageOriginalCenter.x + translation.x, y: messageOriginalCenter.y)
+            
+            
+        
+        }else if sender.state == UIGestureRecognizerState.Ended {
+            
+            print("ended")
+            
+        }
+    }
 }
